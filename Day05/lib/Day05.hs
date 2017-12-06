509e45c55143c8aa7030e4efa089a98eb3a5b935
Jonathan Taylor <jont@cakesolutions.net>
2017-12-06 00:35:17 +0000

    Complete part 1 of day 5

--- Day05/lib/Day05.hs
+++ Day05/lib/Day05.hs
@@ -1,4 +1,22 @@
 module Day05 where
 
+import           Data.Map (Map)
+import qualified Data.Map as Map
+
 escapeMaze :: [Int] -> Int
-escapeMaze jumps = 0
+escapeMaze jumps =
+  loop m 0 0
+  where
+    m = Map.fromList $ zip [0 :: Int ..] jumps
+
+loop :: Map Int Int -> Int -> Int -> Int
+loop m curr steps =
+  case curr' of
+    _ | curr' < 0 || curr' >= Map.size m -> steps'
+    _ -> loop m' curr' steps'
+  where
+    jump = m Map.! curr
+    jump' = succ jump
+    curr' = curr + jump
+    steps' = succ steps
+    m' = Map.insert curr jump' m
--- Day05/src/Main.hs
+++ Day05/src/Main.hs
@@ -2,8 +2,8 @@ import           Day05
 
 main :: IO ()
 main = do
-    input <- readFile "Day05/src/input.txt"
-    let jumps = map read . lines $ input :: [Int]
+  input <- readFile "Day05/src/input.txt"
+  let jumps = map read . lines $ input :: [Int]
 
-    let answer1 = escapeMaze jumps
-    putStrLn $ "answer1: " ++ show answer1
+  let answer1 = escapeMaze jumps
+  putStrLn $ "answer1: " ++ show answer1
