bcc7a98901d7d7d374c89fcc42f8d42f06f85a21
Jonathan Taylor <jont@cakesolutions.net>
2017-12-14 00:17:46 +0000

    Tweaks to part 2 of day 13

--- Day13/index.js
+++ Day13/index.js
@@ -18,21 +18,22 @@ const parseString = s => {
 };
 
 const initialPositions = (m, delay) =>
-    new Map(Array.from(m.keys()).map(k => {
-        const depth = m.get(k);
-        const n = depth - 1;
-        const q = Math.floor(delay / n);
-        const r = delay % n;
+    new Map(Array.from(m.keys()).map(range => {
+        const depth = m.get(range);
+        const depthLessOne = depth - 1;
+        const q = Math.floor(delay / depthLessOne);
+        const r = delay % depthLessOne;
         const isEven = q % 2 === 0;
         const dir = isEven;
-        const pos = isEven ? r : n - r;
-        return [k, { depth, pos, dir }]
+        const pos = isEven ? r : depthLessOne - r;
+        const scanner = { depth, range, pos, dir };
+        return [range, scanner]
     }))
 
-const detectCatches = (acc, tick) => {
-    const scanner = acc.positions.get(tick);
+const detectCatches = (acc, range) => {
+    const scanner = acc.positions.get(range);
     if (scanner && scanner.pos === 0) {
-        acc.catches.push({ depth: scanner.depth, range: tick });
+        acc.catches.push(scanner);
     }
 };
 
@@ -44,16 +45,16 @@ const moveScanners = positions => {
             const changedDir = pos === depth - 1;
             const newPos = changedDir ? pos - 1 : pos + 1;
             const newDir = changedDir ? !dir : dir;
-            const newValue = { pos: newPos, dir: newDir, depth };
-            positions.set(range, newValue);
+            const newScanner = { depth, range, pos: newPos, dir: newDir };
+            positions.set(range, newScanner);
         }
         else {
             // scanning up (pos decreasing)
             const changedDir = pos === 0;
             const newPos = changedDir ? pos + 1 : pos - 1;
             const newDir = changedDir ? !dir : dir;
-            const newValue = { pos: newPos, dir: newDir, depth };
-            positions.set(range, newValue);
+            const newScanner = { depth, range, pos: newPos, dir: newDir,  };
+            positions.set(range, newScanner);
         }
     }
 };
@@ -68,9 +69,9 @@ const crossFirewall = (m, delay) => {
         moveScanners(acc.positions);
         return acc;
     };
-    const maxClock = Math.max(...Array.from(m.keys()));
-    const ticks = Array.from(Array(maxClock + 1).keys());
-    const finalAcc = ticks.reduce(op, seed);
+    const maxRange = Math.max(...Array.from(m.keys()));
+    const ranges = Array.from(Array(maxRange + 1).keys());
+    const finalAcc = ranges.reduce(op, seed);
     return finalAcc.catches;
 };
 
@@ -118,7 +119,7 @@ const real = () => {
             const input = buffer.toString();
             const map = parseString(input);
             console.log(`[real input] part1: ${computePart1(map)}`);
-            // console.log(`[real input] part2: ${computePart2(map)}`);
+            console.log(`[real input] part2: ${computePart2(map)}`);
         }
     });
 };
