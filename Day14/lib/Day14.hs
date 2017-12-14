0d71ab7dfdf5882ccf5f34e91e5294ac9107dbd2
Jonathan Taylor <jont@cakesolutions.net>
2017-12-14 23:25:18 +0000

    Third round of tidy up re day 14

--- Day14/lib/Day14.hs
+++ Day14/lib/Day14.hs
@@ -1,14 +1,12 @@
 module Day14 where
 
-import           Data.List  (nub)
-import           Data.Map   (Map)
-import qualified Data.Map   as Map
-import           Data.Maybe (mapMaybe)
+import           Data.List (nub)
+import           Data.Map  (Map)
+import qualified Data.Map  as Map
 import           Day10
--- import           Text.Printf
 
 squaresUsed :: String -> Int
-squaresUsed s = length $ filter (== '1') $ mkGrid s
+squaresUsed = length . filter (== '1') . mkGrid
 
 countRegions :: String -> Int
 countRegions s = rids
@@ -17,27 +15,29 @@ countRegions s = rids
     seed = (Map.empty, 0)
     range = [0..127]
     positions = [y * 128 + x | y <- range, x <- range]
-    op acc @ (m, rid) pos =
-      if isUsed pos
-        then case Map.lookup pos m of
-          Just _  -> acc
-          Nothing -> (m', rid')
-            where
-              ns = deepNeighbours [pos] [pos]
-              m2 = Map.fromList $ map (\n -> (n, rid')) ns
-              m' = Map.union m m2
-              rid' = succ rid
-        else acc
-    deepNeighbours ps ns =
-      case nsnew5 of
-        [] -> ns
-        _  -> deepNeighbours nsnew5 (ns ++ nsnew5)
+    op acc @ (m, rid) pos = if isUsed pos
+      then case Map.lookup pos m of
+        Just _  -> acc
+        Nothing -> (m', rid')
+          where
+            ns = deepNeighbours [pos] [pos]
+            m2 = Map.fromList $ map (flip (,) rid') ns
+            m' = Map.union m m2
+            rid' = succ rid
+      else acc
+    deepNeighbours ps nsCurr =
+      case nsNext of
+        [] -> nsCurr
+        _  -> deepNeighbours nsNext (nsCurr ++ nsNext)
       where
-        nsnew1 = concatMap immediateNeighbours ps
-        nsnew2 = nub nsnew1
-        nsnew3 = filter (\n -> n >= 0 && n < 16384) nsnew2
-        nsnew4 = filter isUsed nsnew3
-        nsnew5 = filter (`notElem` ns) nsnew4
+        nsNext =
+          filter isUsed .
+          filter inRange .
+          filter notSeen .
+          nub $
+          concatMap immediateNeighbours ps
+        notSeen p = p `notElem` nsCurr
+        inRange p = p >= 0 && p < 128 * 128
         immediateNeighbours p =
           case p `mod` 128 of
             0   -> [p + 1, p + 128, p - 128] -- left edge
