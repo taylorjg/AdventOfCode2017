40b5d182a81f3a5880506aec99ec125f519c5435
Jonathan Taylor <jont@cakesolutions.net>
2017-12-17 09:19:10 +0000

    Complete part 2 of day 17

--- Day17/index.js
+++ Day17/index.js
@@ -1,8 +1,8 @@
-const spinlock = steps => {
+const spinlock1 = steps => {
     const buffer = [0];
     let pos = 0;
     for (let i = 1; i <= 2017; i++) {
-        const len = buffer.length;
+        const len = i;
         const insertPos = (pos + steps) % len;
         pos = insertPos + 1;
         buffer.splice(pos, 0, i);
@@ -10,14 +10,36 @@ const spinlock = steps => {
     return buffer[pos + 1];
 };
 
+const spinlock2 = steps => {
+    let pos = 0;
+    let zeroPos = 0;
+    let result = -1;
+    for (let i = 1; i <= 50000000; i++) {
+        pos = (pos + steps) % i + 1;
+        if (pos === 0) {
+            // Keep track of where 0 is.
+            zeroPos++;
+        }
+        if (pos === zeroPos + 1) {
+            // Remember the value inserted immediately after 0.
+            result = i;
+        }
+    }
+    return result;
+};
+
 const test = () => {
-    const result = spinlock(3);
-    console.log(`[test] part1: ${result}`);
+    const result1 = spinlock1(3);
+    console.log(`[test] part1: ${result1}`);
+    const result2 = spinlock2(3);
+    console.log(`[test] part2: ${result2}`);
 };
 
 const real = () => {
-    const result = spinlock(301);
-    console.log(`[real] part1: ${result}`);
+    const result1 = spinlock1(301);
+    console.log(`[real] part1: ${result1}`);
+    const result2 = spinlock2(301);
+    console.log(`[real] part2: ${result2}`);
 };
 
 test();
