538bd12fc311e978c035abf1d73661620a3c599d
Jonathan Taylor <jont@cakesolutions.net>
2017-12-22 23:34:46 +0000

    Complete part 2 of day 22

--- Day22/index.js
+++ Day22/index.js
@@ -20,15 +20,6 @@ const parseInput = input => {
 const equalCoords = c1 => c2 =>
     c1.x === c2.x && c1.y === c2.y;
 
-const calculateNewDirection = (oldDir, infected) => {
-    switch (oldDir) {
-        case "U": return infected ? "R" : "L";
-        case "D": return infected ? "L" : "R";
-        case "L": return infected ? "U" : "D";
-        case "R": return infected ? "D" : "U";
-    }
-};
-
 const calculateNewPosition = ({ x, y }, dir) => {
     switch (dir) {
         case "U": return { x, y: y + 1 };
@@ -47,28 +38,134 @@ const removeFromMap = (map, pos) => {
 };
 
 const addToMap = (map, pos) => {
-    const newMap = map.slice();
-    newMap.push(pos);
-    return newMap;
-};
-
-const reducer = state => {
-    const oldInfected = !!state.map.find(equalCoords(state.pos));
-    const newInfected = !oldInfected;
-    const newDir = calculateNewDirection(state.dir, oldInfected);
-    const newPos = calculateNewPosition(state.pos, newDir);
-    const newMap = (newInfected) ? addToMap(state.map, state.pos) : removeFromMap(state.map, state.pos)
-    return {
-        map: newMap,
-        pos: newPos,
-        dir: newDir,
-        numInfectingBursts: state.numInfectingBursts + (newInfected ? 1 : 0)
-    };
+    map.push(pos);
 };
 
 const computePart1 = (map, numIterations) => {
+
+    const calculateNewDirection = (oldDir, infected) => {
+        switch (oldDir) {
+            case "U": return infected ? "R" : "L";
+            case "D": return infected ? "L" : "R";
+            case "L": return infected ? "U" : "D";
+            case "R": return infected ? "D" : "U";
+        }
+    };
+
+    const reducer = state => {
+        const oldInfected = !!state.map.find(equalCoords(state.pos));
+        const newInfected = !oldInfected;
+        const newDir = calculateNewDirection(state.dir, oldInfected);
+        const newPos = calculateNewPosition(state.pos, newDir);
+
+        if (newInfected) {
+            addToMap(state.map, state.pos);
+        }
+        else {
+            removeFromMap(state.map, state.pos);
+        }
+
+        return {
+            map: state.map,
+            pos: newPos,
+            dir: newDir,
+            numInfectingBursts: state.numInfectingBursts + (newInfected ? 1 : 0)
+        };
+    };
+
     const initialState = {
         map,
+        pos: { x: 0, y: 0 },
+        dir: "U",
+        numInfectingBursts: 0
+    };
+    const range = Array.from(Array(numIterations).keys());
+    const finalState = range.reduce(reducer, initialState);
+    return finalState.numInfectingBursts;
+};
+
+const computePart2 = (map, numIterations) => {
+
+    const CLEAN = 0;
+    const INFECTED = 1;
+    const WEAKENED = 2;
+    const FLAGGED = 3;
+
+    const TABLE = {
+        "U": {
+            [CLEAN]: "L",
+            [WEAKENED]: "U",
+            [INFECTED]: "R",
+            [FLAGGED]: "D"
+        },
+        "D": {
+            [CLEAN]: "R",
+            [WEAKENED]: "D",
+            [INFECTED]: "L",
+            [FLAGGED]: "U"
+        },
+        "L": {
+            [CLEAN]: "D",
+            [WEAKENED]: "L",
+            [INFECTED]: "U",
+            [FLAGGED]: "R"
+        },
+        "R": {
+            [CLEAN]: "U",
+            [WEAKENED]: "R",
+            [INFECTED]: "D",
+            [FLAGGED]: "L"
+        }
+    };
+
+    const calculateNewDirection = (oldDir, oldState) => TABLE[oldDir][oldState];
+
+    const calculateNewState = oldState => {
+        switch (oldState) {
+            case CLEAN: return WEAKENED;
+            case WEAKENED: return INFECTED;
+            case INFECTED: return FLAGGED;
+            case FLAGGED: return CLEAN;
+        }
+    };
+
+    const reducer = (state, iteration) => {
+        if (iteration % 10000 === 0) {
+            console.log(`iteration: ${iteration}`);
+        }
+        const elem = state.map.find(equalCoords(state.pos));;
+        const oldState = elem ? elem.state : CLEAN;
+        const newState = calculateNewState(oldState);
+        const newDir = calculateNewDirection(state.dir, oldState);
+        const newPos = calculateNewPosition(state.pos, newDir);
+        
+        if (newState === CLEAN) {
+            removeFromMap(state.map, elem);
+        }
+        else {
+            if (elem) {
+                elem.state = newState;
+            }
+            else {
+                const newElem = {
+                    x: state.pos.x,
+                    y: state.pos.y,
+                    state: newState
+                };
+                addToMap(state.map, newElem);
+            }
+        }
+
+        return {
+            map: state.map,
+            pos: newPos,
+            dir: newDir,
+            numInfectingBursts: state.numInfectingBursts + (newState === INFECTED ? 1 : 0)
+        };
+    };
+
+    const initialState = {
+        map: map.map(({ x, y }) => ({ x, y, state: INFECTED })),
         pos: { x: 0, y: 0 },
         dir: "U",
         numInfectingBursts: 0
@@ -85,9 +182,12 @@ const run = (fileName, label, numIterations) => {
         }
         else {
             const input = buffer.toString();
-            const map = parseInput(input);
-            const answer1 = computePart1(map, numIterations);
-            console.log(`[${label} input (${numIterations} iterations)] answer1: ${answer1}`);
+            // const map1 = parseInput(input);
+            // const answer1 = computePart1(map1, numIterations);
+            // console.log(`[${label} input (${numIterations} iterations)] answer1: ${answer1}`);
+            const map2 = parseInput(input);
+            const answer2 = computePart2(map2, numIterations);
+            console.log(`[${label} input (${numIterations} iterations)] answer2: ${answer2}`);
         }
     });
 };
@@ -95,7 +195,10 @@ const run = (fileName, label, numIterations) => {
 const test = numIterations => run("Day22/test.txt", "test", numIterations);
 const real = numIterations => run("Day22/input.txt", "real", numIterations);
 
-test(7);
-test(70);
-test(10000);
-real(10000);
+// test(7);
+// test(70);
+// test(10000);
+// real(10000);
+// test(100);
+// test(10000000);
+real(10000000);
