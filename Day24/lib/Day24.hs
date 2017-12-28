8768101b8ff93ae3535983bd08bc3139a7751e3f
Jonathan Taylor <jont@cakesolutions.net>
2017-12-28 15:50:58 +0000

    More prep for day 24 in Haskell

--- Day24/lib/Day24.hs
+++ Day24/lib/Day24.hs
@@ -1,12 +1,40 @@
 module Day24 where
 
+import           Data.List.Split (splitOn)
+import           Debug.Trace
+
 data Component = Component { a :: Int, b :: Int } deriving (Show, Eq, Ord)
 
+type Bridge = [Component]
+
+parseComponent :: String -> Component
+parseComponent line = Component a b
+  where
+    bits = splitOn "/" line
+    a = read $ head bits
+    b = read $ last bits
+
 parseInput :: String -> [Component]
-parseInput input = []
+parseInput input = parseComponent `fmap` ls
+  where
+    ls = filter (not . null) $ splitOn "\n" input
+
+bridgeStrength :: Bridge -> Int
+bridgeStrength = foldl (\acc c -> acc + a c + b c) 0
+
+validBridges :: [Component] -> [Bridge]
+validBridges components = [[Component 0 1, Component 1 2, Component 2 3], [Component 50 50]]
 
 computePart1 :: [Component] -> Int
-computePart1 components = 1
+computePart1 components =
+  maximum $ map bridgeStrength bridges
+  where
+    bridges = validBridges components
 
 computePart2 :: [Component] -> Int
-computePart2 components = 2
+computePart2 components =
+  maximum $ map bridgeStrength longestBridges
+  where
+    bridges = validBridges components
+    maxLength = maximum $ map length bridges
+    longestBridges = filter ((== maxLength) . length) bridges
--- Day24/test/Spec.hs
+++ Day24/test/Spec.hs
@@ -7,8 +7,14 @@ main = hspec $
   describe "Day24 tests" $ do
 
     let components = [
-            Component 0 0,
-            Component 1 1
+            Component 0 2,
+            Component 2 2,
+            Component 2 3,
+            Component 3 4,
+            Component 3 5,
+            Component 0 1,
+            Component 10 1,
+            Component 9 10
           ]
 
     describe "Part 1" $ do
@@ -18,9 +24,9 @@ main = hspec $
         parseInput input `shouldBe` components
 
       it "can compute part 1" $
-        computePart1 components `shouldBe` 0
+        computePart1 components `shouldBe` 31
 
     describe "Part 2" $
 
       it "can compute part 2" $
-        computePart2 components `shouldBe` 0
+        computePart2 components `shouldBe` 19
