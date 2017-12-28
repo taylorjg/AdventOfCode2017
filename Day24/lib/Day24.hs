f9faf40b5598638919c5d2fb2d24ea12e3dd5894
Jonathan Taylor <jont@cakesolutions.net>
2017-12-28 19:16:07 +0000

    Most of day 24 done in Haskell

--- Day24/lib/Day24.hs
+++ Day24/lib/Day24.hs
@@ -1,5 +1,6 @@
 module Day24 where
 
+import           Data.List       ((\\))
 import           Data.List.Split (splitOn)
 import           Debug.Trace
 
@@ -23,16 +24,40 @@ bridgeStrength :: Bridge -> Int
 bridgeStrength = foldl (\acc c -> acc + a c + b c) 0
 
 validBridges :: [Component] -> [Bridge]
-validBridges components = [[Component 0 1, Component 1 2, Component 2 3], [Component 50 50]]
+validBridges components = go startingBridges
+  where
+    startingBridges = map (:[]) startingComponents
+    startingComponents = filter (canConnect 0) components
+    go bridges =
+      case nextBridges of
+        [] -> bridges
+        _  -> go nextBridges
+      where
+        nextBridges = concatMap advance bridges
+        advance bridge = map (:bridge) matches
+          where
+            rest = components \\ bridge
+            port = lastUnmatchedPort bridge
+            matches = filter (canConnect port) rest
+    canConnect port component = a component == port || b component == port
+    lastUnmatchedPort bridge =
+      case bridge of
+        Component a b:Component c d:_ | a == c || a == d -> b
+        Component a b:Component c d:_ | b == c || b == d -> a
+        [Component a b]               | a == 0               -> b
+        [Component a b]               | b == 0               -> a
+        []                            -> 0
 
 computePart1 :: [Component] -> Int
 computePart1 components =
+  trace ("bridges: " ++ show bridges) $
   maximum $ map bridgeStrength bridges
   where
     bridges = validBridges components
 
 computePart2 :: [Component] -> Int
 computePart2 components =
+  trace ("bridges: " ++ show bridges) $
   maximum $ map bridgeStrength longestBridges
   where
     bridges = validBridges components
